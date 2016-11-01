d138ff8384f332ca7043896ff8b488123f21362b
Matt Glover <matthewglover@me.com>
2016-11-01 12:58:08 +0000

    feat(map): implement map

--- future.js
+++ future.js
@@ -5,6 +5,11 @@ export default class Future {
     this.__f = f;
   }
 
+  map(f) {
+    return new Future((reject, resolve) =>
+      this.fork(reject, x => resolve(f(x))));
+  }
+
   fork(reject, resolve) {
     this.__f(reject, resolve);
   }
